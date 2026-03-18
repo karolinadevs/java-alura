a768a5fae05a010e7dca600f1e36f710ef4b15ff
anna <naksilva23@gmail.com>
2026-03-18 14:08:49 -0300

    adicionando streaming

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("esse é o screen match.");
+        System.out.println("filme: toy story 2");
+
+        int anoDeLancamento = 1999;
+        System.out.println("ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+    }
+}
+
+
