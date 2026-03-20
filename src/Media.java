007cd963bfaa8f8a5284fb3e97443ee8f40ad47c
anna <jkorumaito@gmail.com>
2026-03-19 23:48:17 -0300

    corrigindo para public class

--- src/Media.java
+++ src/Media.java
@@ -1,7 +1,10 @@
-void main() {
-    double nota1 = 8.9;
-    double nota2 = 2;
-    double media = (nota1 + nota2) / 2;
-    System.out.println("a media é: " + media);
+public class Media {
+    public static void main(String[] args){
+        double nota1 = 8.9;
+        double nota2 = 2;
+        double media = (nota1 + nota2) / 2;
+        System.out.println("a media é: " + media);
+    }
+
 }
 
