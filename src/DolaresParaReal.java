89ca4f712c39dbd431abbe84bce34376f83d3864
anna <jkorumaito@gmail.com>
2026-03-19 23:57:25 -0300

    desafio converter dolar para real

---
+++ src/DolaresParaReal.java
@@ -0,0 +1,10 @@
+public class DolaresParaReal {
+    public static void main(String[] args) {
+        double valorEmDolares = 100.50;
+        double taxaDeConversao = 4.94;
+
+        double valorEmReais = valorEmDolares * taxaDeConversao;
+
+        System.out.println("O valor em reais é: " + valorEmReais);
+    }
+}
