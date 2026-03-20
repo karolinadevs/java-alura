49d0e6fece0e577316715144f486b19538b746e9
anna <jkorumaito@gmail.com>
2026-03-19 23:46:49 -0300

    desafio casting double para int

---
+++ src/Casting.java
@@ -0,0 +1,7 @@
+public class Casting {
+    public static void main(String[] args){
+        double numeroDouble = 2.3;
+        int numeroInt = (int) numeroDouble;
+        System.out.println("casting de double para int: " + numeroInt);
+    }
+}
