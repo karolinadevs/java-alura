0c8a5e5dbf92550e9acec591a41a3c488d307928
anna <jkorumaito@gmail.com>
2026-03-20 00:13:37 -0300

    segundo desafio: mostrar as horas do computador em tempo real

--- src/JavaCev.java
+++ src/JavaCev.java
@@ -2,6 +2,9 @@ import java.util.Date;
 
 public class JavaCev {
     public static void main(String[] args){
+
+
+
         Date relogio = new Date();
         System.out.println("a hora do sitema eh: ");
         System.out.println(relogio.toString());
