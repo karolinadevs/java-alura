8637bfaddc0854fb1d014f2cbb9066906532d0a7
anna <jkorumaito@gmail.com>
2026-03-20 00:13:01 -0300

    passando dois arquivos do repo 'java_cev' para ceste

---
+++ src/JavaCev.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class JavaCev {
+    public static void main(String[] args){
+        Date relogio = new Date();
+        System.out.println("a hora do sitema eh: ");
+        System.out.println(relogio.toString());
+    }
+}
+
