2e6fac775787656c8bf162add31a4cf90804c86b
anna <jkorumaito@gmail.com>
2026-03-19 23:54:34 -0300

    desafio atribuir valores e concatenar em uma mensagem

---
+++ src/Palavras.java
@@ -0,0 +1,9 @@
+public class Palavras {
+    public static void main(String[] args){
+        char letra = 'A';
+        String palavra = "marelo";
+        String mensagem = "a letra é " + letra + " e a palavra é " + palavra;
+        System.out.println(mensagem);
+        System.out.println("agora letra com palavra formam: "+ letra + palavra);
+    }
+}
