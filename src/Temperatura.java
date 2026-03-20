9ec1e00bcde0d3fb478b5f705d70d67503091f5e
anna <jkorumaito@gmail.com>
2026-03-19 23:36:21 -0300

    adicionando conversao de celsius para fahrenheit + casting

--- src/Temperatura.java
+++ src/Temperatura.java
@@ -2,5 +2,11 @@ public class Temperatura {
     public static void main(String [] args){
         double celsius = 23.1;
         double fahrenheit = (celsius * 1.8) + 32;
+
+        String mensagem = String.format("a temperatura de %.1f celsius é equivalente a %.1f fahrenheit", celsius, fahrenheit);
+        System.out.println(mensagem);
+
+        int fahrenheitInteira = (int) fahrenheit;
+        System.out.println("a temperatura de fahrenheit inteira é: " + fahrenheitInteira);
     }
 }
