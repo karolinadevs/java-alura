50c8a0e1dc5b8ac35722ac40f641828a35299ccc
anna <jkorumaito@gmail.com>
2026-03-19 21:53:09 -0300

    adicionando media da nota do filme "toy story 2"

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
 
+        double media = (10.0 + 10.0)/2;
+        System.out.println(media);
     }
 }
 
