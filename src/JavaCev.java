8c808ec053090e7dabdb3e326f97f2d5dda1096d
anna <jkorumaito@gmail.com>
2026-03-20 00:14:11 -0300

    dando hello world

--- src/JavaCev.java
+++ src/JavaCev.java
@@ -3,7 +3,7 @@ import java.util.Date;
 public class JavaCev {
     public static void main(String[] args){
 
-
+        System.out.print("ola, mundo!");
 
         Date relogio = new Date();
         System.out.println("a hora do sitema eh: ");
