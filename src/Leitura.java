8afa12ade5f3365451a2142b9856a6cfec59e725
anna <jkorumaito@gmail.com>
2026-03-20 22:30:36 -0300

    pedindo ano de lancamento, avaliacao e exibindo

--- src/Leitura.java
+++ src/Leitura.java
@@ -5,6 +5,15 @@ public class Leitura {
         Scanner leitura = new Scanner(System.in);
         System.out.println("digite seu filme favorito: ");
         String filme = leitura.nextLine();
+
+        System.out.println("qual o ano de lançamento?");
+        int anoDeLancamento = leitura.nextInt();
+
+        System.out.println("diga sua avaliacao para o filme de 1 a 10: ");
+        double avaliacao = leitura.nextDouble();
+
         System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacao);
     }
 }
