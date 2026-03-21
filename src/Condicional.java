a4034326186c015d70417641b33aa20f2b9a4bd1
anna <jkorumaito@gmail.com>
2026-03-20 22:11:50 -0300

    plano de assinatura

--- src/Condicional.java
+++ src/Condicional.java
@@ -3,11 +3,18 @@ public class Condicional {
         int anoDeLancamento = 2022;
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+        String tipoPlano = "normal";
 
         if (anoDeLancamento >= 2020) {
             System.out.println("lançamento que os clientes estão curtindo!");
         } else {
             System.out.println("filme retro que vale a pena assistir!");
+        }
+
+        if (incluidoNoPlano == true || tipoPlano.equals("plus")) {
+            System.out.println("filme liberado");
+        } else {
+            System.out.println("pagar alocação");
         }
     }
 }
