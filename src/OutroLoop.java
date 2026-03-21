6bd6d899feb44d069582d839cd8e4e0da7a208d4
anna <jkorumaito@gmail.com>
2026-03-20 22:52:52 -0300

    usando loop com while e if

--- src/OutroLoop.java
+++ src/OutroLoop.java
@@ -5,12 +5,18 @@ public class OutroLoop {
         Scanner leitura = new Scanner(System.in);
         double mediaAvaliacao = 0;
         double nota = 0;
+        int totalNotas = 0;
 
         while (nota != -1) {
             System.out.println("diga sua avaliacao para o filme ou -1 para encerrar: ");
             nota = leitura.nextDouble();
-            mediaAvaliacao += nota;
+
+            if (nota != -1) {
+                mediaAvaliacao += nota;
+                totalNotas++; //a cada nota que for colocado, vai aumentar o totalNotas
+            }
         }
-        System.out.println("media de avaliacoes " + mediaAvaliacao);
+        System.out.println("media de avaliacoes " + mediaAvaliacao / totalNotas);
     }
 }
+
