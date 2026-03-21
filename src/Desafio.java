ad57f89a88b7d3f53ed46a54a59dd7a99797d79b
anna <jkorumaito@gmail.com>
2026-03-20 23:47:50 -0300

    desafio: interface de uma conta bancaria

---
+++ src/Desafio.java
@@ -0,0 +1,11 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "joji";
+        String tipoConta = "corrente";
+        double saldo = 10500;
+
+        System.out.println("\nnome do cliente: " + nome);
+        System.out.println("tipo conta: " + tipoConta);
+        System.out.println("saldo atual: " + saldo);
+    }
+}
