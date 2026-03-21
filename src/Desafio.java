217436bf4324701ba237fbbab69f722080ac315e
anna <jkorumaito@gmail.com>
2026-03-20 23:58:58 -0300

    visualizacao do saldo, teste do codigo

--- src/Desafio.java
+++ src/Desafio.java
@@ -25,6 +25,25 @@ public class Desafio {
             System.out.println(menu);
             opcao = leitura.nextInt();
 
+            if (opcao == 1) {
+                System.out.println("o saldo atualizado é " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("qual o valor deseja transferir?");
+                double valor = leitura.nextDouble();
+                if (valor > saldo) {
+                    System.out.println("não saldo suficiente");
+                } else {
+                    saldo -= valor;
+                    System.out.println("novo saldo " + saldo);
+                }
+            } else if (opcao == 3) {
+                System.out.println("valor recebido: ");
+                double valor = leitura.nextDouble();
+                saldo += valor;
+                System.out.println("novo saldo " + saldo);
+            } else if (opcao != 4) {
+                System.out.println("opcao invalida");
+            }
         }
     }
 }
