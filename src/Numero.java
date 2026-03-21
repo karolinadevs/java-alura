68df2d288337acfac3f42399e855679b5d9c6227
anna <jkorumaito@gmail.com>
2026-03-20 23:19:41 -0300

    um programa que pede um numero pro usuario e informa se é positivo ou negativo

---
+++ src/Numero.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Numero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("digite um numero: ");
+        int numero = scanner.nextInt();
+
+        if (numero > 0) {
+            System.out.println("esse numero é positivo");
+        } else {
+            System.out.println("esse numero é negativo");
+        }
+    }
