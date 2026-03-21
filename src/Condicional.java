c7f72b703adc0473668552f21188cf868185e488
anna <jkorumaito@gmail.com>
2026-03-20 22:05:29 -0300

    adicionando 'condicional - if else'

---
+++ src/Condicional.java
@@ -0,0 +1,13 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        if (anoDeLancamento >= 2020) {
+            System.out.println("lançamento que os clientes estão curtindo!");
+        } else {
+            System.out.println("filme retro que vale a pena assistir!");
+        }
+    }
+}
